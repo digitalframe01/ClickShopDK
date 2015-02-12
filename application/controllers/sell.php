49839cda66b4b623125c299760d5cf9ff36a5ec0
digitalframe01 <digitalframe01@gmail.com>
2015-02-11 21:26:06 -0800

    - added sell.php file to controllers to view product post

--- application/controllers/sell.php
+++ application/controllers/sell.php
@@ -1,7 +1,14 @@
 <?php
 
-/* 
+/*
  * This site is developed and maintained by DigitalFrame
  * E-mail: digitalframe01@outlook.com
  */
 
+class Sell extends CI_Controller {
+
+    function post() {
+        $this->load->view('product_post');
+    }
+
+}
