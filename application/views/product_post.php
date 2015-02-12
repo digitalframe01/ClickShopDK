de857081fbd1dcdd62a507f815eab88e51d8e1c9
digitalframe01 <digitalframe01@gmail.com>
2015-02-11 21:30:24 -0800

    - added product_post.php as views to show product posting page

--- application/views/product_post.php
+++ application/views/product_post.php
@@ -1,7 +1,83 @@
-<?php
+<?php ?>
 
 /* 
  * This site is developed and maintained by DigitalFrame
  * E-mail: digitalframe01@outlook.com
  */
+ 
+ 
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<title>Post your ad on free classified site of Bangladesh</title>
+<link rel="stylesheet" type="text/css" href="<? echo base_url();?>includes/css/category_menu.css">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta http-equiv="content-language" content="EN">
 
+<!--[if IE 8]>
+		<LINK rel="stylesheet" href="http://static.clickbd.com/global/css/clickbd.ie8.css" type="text/css" media="screen" />
+	<![endif]-->
+<!--[if IE 7]>
+		<LINK rel="stylesheet"  href="http://static.clickbd.com/global/css/clickbd.ie7.css" type="text/css" media="screen" />
+	<![endif]-->
+<!--[if IE 6]>
+		<LINK rel="stylesheet" href="http://static.clickbd.com/global/css/clickbd.ie6.css" type="text/css" media="screen" />
+	<![endif]-->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script src="<? echo base_url();?>includes/js/category_menu.js"></script>
+   
+<script>eval(mod_pagespeed_KcrF6nu9VS);</script>
+<script>eval(mod_pagespeed_xWmye_2qtW);</script>
+<script>eval(mod_pagespeed_XonePe56TZ);</script>
+</head>
+<body class="body_bg_water">
+
+<div id="wrapper">
+<div class="section curve" id="choose_category">
+
+<div class="clr"></div>
+<div id="category-lists">
+<ul id="categories" class="level-one" style="margin-left: 0px;">
+    <li class=""><a href="#1" class="toggle-link">Agro &amp; Livestock</a></li>
+    <li><a href="#18" class="toggle-link">Cars &amp; Vehicles</a></li>
+    <li class="active"><a href="#72" class="toggle-link">Events</a></li>
+    <li class=""><a href="#80" class="toggle-link">For Sale</a></li>
+    <li><a href="#267" class="toggle-link">JOBS</a></li>
+    <li><a href="#299" class="toggle-link">Properties &amp; Rents</a></li>
+    <li><a href="#328" class="toggle-link">Services</a></li>
+</ul>
+    <ul id="1" class="level-two category" style="display: none;">
+    <li class=""><a href="#2" class="final-level toggle-link">Corn</a></li>
+    <li><a href="#3" class="final-level toggle-link">Fruit</a></li>
+    <li><a href="#4" class="final-level toggle-link">Mushroom</a></li>
+    <li><a href="#5" class="final-level toggle-link">Plants</a></li>
+    <li><a href="#6" class="final-level toggle-link">Poultry &amp; Live stocks</a></li>
+    <li><a href="#7" class="final-level toggle-link">Rice</a></li>
+    <li><a href="#8" class="final-level toggle-link">Shrimp</a></li>
+    <li class=""><a href="#9" class="toggle-link">Supplies</a></li>
+    <li><a href="#13" class="final-level toggle-link">Vegetable</a></li>
+    <li><a href="#14" class="final-level toggle-link">Wheat</a></li>
+    <li><a href="#15" class="final-level toggle-link">Fisheries</a></li>
+    <li><a href="#16" class="final-level toggle-link">Dairy</a></li>
+    <li><a href="#17" class="final-level toggle-link">Other Agro &amp; Livestock</a></li>
+    </ul>
+        <ul id="9" class="level-three category" style="display: none;">
+        <li><a href="#10" class="final-level toggle-link">Animal Feeds</a></li>
+        <li class=""><a href="#11" class="final-level toggle-link">Seeds</a></li>
+        <li><a href="#12" class="final-level toggle-link">Renu Pona</a></li>
+        </ul>
+
+<fieldset id="continue">
+<form id="category_form" action="/sell/post_an_ad/" method="get">
+<input value="#77" name="category_id" id="category_id" type="hidden">
+<div id="submit" class="warning ready">
+<button type="submit" name="continue" id="category_continue" class="button1 bgreen">Continue</button>
+<div id="warning-note" class="note">
+<p>
+Please select a category before continuing.
+</p>
+</div>
+</div>
+</form>
+</fieldset>
+</div>
+    
+ </body></html>
